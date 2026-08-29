8f2bdfa0073df3a09719b75c528200c5e6ecad31
jhyxng9088 <j.hyxng9088@gmail.com>
2026-08-29 16:41:49 +0900

    Add Firebase client connection

---
+++ src/firebase.js
@@ -0,0 +1,33 @@
+import { initializeApp } from 'firebase/app'
+import { browserLocalPersistence, getAuth, setPersistence, signInAnonymously } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD4F5hQItDGTGItXJ2vnuu7ExM1LBLn9E0',
+  authDomain: 'school-adeda.firebaseapp.com',
+  projectId: 'school-adeda',
+  storageBucket: 'school-adeda.firebasestorage.app',
+  messagingSenderId: '321702677113',
+  appId: '1:321702677113:web:390c5d63e3d93ec17f22a8',
+  measurementId: 'G-PFCP63TWQS',
+}
+
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+
+let sessionPromise = null
+export async function ensureAdminSession() {
+  if (auth.currentUser) return auth.currentUser
+  if (!sessionPromise) {
+    sessionPromise = (async () => {
+      try { await setPersistence(auth, browserLocalPersistence) } catch {}
+      const credential = await signInAnonymously(auth)
+      return credential.user
+    })().catch((error) => {
+      sessionPromise = null
+      throw error
+    })
+  }
+  return sessionPromise
+}
